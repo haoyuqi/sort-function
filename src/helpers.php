f23f71829f5f054eaaf7befdd71e0921dcaa7f27
Hao Yuqi <hi.haoyuqi+github@gmail.com>
2020-03-13 20:58:43 +0800

    add select_sort functions

--- src/helpers.php
+++ src/helpers.php
@@ -55,3 +55,36 @@ if (!function_exists('quick_sort')) {
         return array_merge(quick_sort($leftArray), [$middle], quick_sort($rightArray));
     }
 }
+
+if (!function_exists('select_sort')) {
+    /**
+     * 选择排序
+     * @param array $arr
+     * @return array
+     */
+    function select_sort(array $arr)
+    {
+        $count = count($arr);
+        if ($count < 2) {
+            return $arr;
+        }
+
+        for ($i = 0; $i < $count - 1; $i++) {
+            $key = $i;
+
+            for ($k = $i + 1; $k < $count; $k++) {
+                if ($arr[$key] > $arr[$k]) {
+                    $key = $k;
+                }
+            }
+
+            if ($key != $i) {
+                $temp = $arr[$key];
+                $arr[$key] = $arr[$i];
+                $arr[$i] = $temp;
+            }
+        }
+
+        return $arr;
+    }
+}
